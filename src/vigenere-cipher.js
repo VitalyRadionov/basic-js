3329ffa19f68e972d995f488d3973f7dec80b59e
Sam Gold <Radionovv89@gmail.com>
2023-04-14 15:09:40 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,16 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor(reverse = true) {
+		this.a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		this.reverse = reverse;
+	}
+
+	encrypt(message, key) {
+		if (message === undefined || key === undefined) {
+			throw new Error('Incorrect arguments!');
+		}
+
+		message = message.toUpperCase();
+		key = key.toUpperCase();
+		let r = '';
+		let j = 0;
+		for (let i = 0; i < message.length; i++) {
+			let mi = this.a.indexOf(message[i]);
+			if (mi >= 0) {
+				let ki_s = key[(j >= key.length) ? j = 0 : j];
+				let ki = this.a.indexOf(ki_s);
+				j++;
+				r += this.a[(this.a.length + (mi + ki)) % this.a.length];
+			} else {
+				r += message[i];
+			}
+		}
+		return this.reverse ? r : r.split('').reverse().join('');
+	}
+	decrypt(encryptedMessage, key) {
+		if (encryptedMessage === undefined || key === undefined) {
+			throw new Error('Incorrect arguments!');
+		}
+
+		encryptedMessage = encryptedMessage.toUpperCase();
+		key = key.toUpperCase();
+
+		let r = '';
+		let j = 0;
+		for (let i = 0; i < encryptedMessage.length; i++) {
+			let mi = this.a.indexOf(encryptedMessage[i]);
+			if (mi >= 0) {
+				let ki_s = key[(j >= key.length) ? j = 0 : j];
+				let ki = -this.a.indexOf(ki_s);
+				j++;
+				r += this.a[(this.a.length + (mi + ki)) % this.a.length];
+			} else {
+				r += encryptedMessage[i];
+			}
+		}
+		return this.reverse ? r : r.split('').reverse().join('');
+	}
 }
 
 module.exports = {
-  VigenereCipheringMachine
+	VigenereCipheringMachine
 };
