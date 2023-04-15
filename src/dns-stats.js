ac14bf6ffbbfd36e85a8d72631c87ead290de78b
Sam Gold <Radionovv89@gmail.com>
2023-04-16 00:55:01 +0300

    feat: dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+	let domObj = {};
+	domains.forEach(e => {
+		e.split('.').reverse().forEach((_, l, arr2) => {
+			let prop = '.' + arr2.slice(0, l + 1).join('.');
+			domObj[prop] = 1 + (domObj[prop] ?? 0);
+		});
+	});
+	return domObj;
 }
 
 module.exports = {
-  getDNSStats
+	getDNSStats
 };
